43363cbdbcc6e1387350191b97d269cde2b0c5ba
manojLuffy <manoz1994k@gmail.com>
2025-01-04 22:29:26 +0530

    removing unused imports

--- src/component/about.js
+++ src/component/about.js
@@ -1,6 +1,3 @@
-import { Dialog, DialogPanel } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import { useState } from "react";
 import Carousel from "./carosel";
 import Image1 from "../pics/image1.jpg";
 import Image2 from "../pics/image2.jpg";
